c2fe32469b621ef238baba654a162ed4a011b60c
GiblerW <westongibler@gmail.com>
2018-11-15 15:35:34 -0700

    switch db connection to cloud and migrated/seeded data

--- knexfile.js
+++ knexfile.js
@@ -4,12 +4,12 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/recipes'
+    connection: 'postgres://iowqbjmosyhglx:7a9588660bb298638c40e5e817ebe025ba2ca1b5eb1b086e59a3ff66d5da6a6c@ec2-54-225-98-131.compute-1.amazonaws.com:5432/d505ppm9m0iise' + '?ssl=true'
   },
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL + '?ssl=true'
+    connection: 'postgres://iowqbjmosyhglx:7a9588660bb298638c40e5e817ebe025ba2ca1b5eb1b086e59a3ff66d5da6a6c@ec2-54-225-98-131.compute-1.amazonaws.com:5432/d505ppm9m0iise' + '?ssl=true'
   }
 
 };
