4f2eb43dd7731b26c18b84d5f263d9274d4e430e
GiblerW <westongibler@gmail.com>
2018-11-15 16:06:08 -0700

    added new start script

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import morgan from 'morgan';
+const express =  require('express');
+const morgan = require('morgan');
 
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000
