c53bd4e716d393914c7a2b78d8ed78174f91f29f
GiblerW <westongibler@gmail.com>
2018-11-15 15:46:11 -0700

    added route name

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('../db/queries');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/recipes', (req, res, next) => {
   db.getAllRecipes()
   .then((recipes) => {
     res.json(recipes);
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000
 // Initialize http server
 const app = express();
 
-var route = require('./routes/index');
+const route = require('./routes/index');
 
 // Logger that outputs all requests into the console
 app.use(morgan('combined'));
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Use v1 as prefix for all API endpoints
-app.use('/v1', route);
+app.use('/', route);
 
 // Launch the server on port 3000
 const server = app.listen(port, () => {
