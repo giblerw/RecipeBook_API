0c7275e7dad01353172340c1c3ed5031c5001d71
GiblerW <westongibler@gmail.com>
2018-11-15 16:12:56 -0700

    Data is now hosted  as API endpoint

--- knexfile.js
+++ knexfile.js
@@ -4,12 +4,12 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://iowqbjmosyhglx:7a9588660bb298638c40e5e817ebe025ba2ca1b5eb1b086e59a3ff66d5da6a6c@ec2-54-225-98-131.compute-1.amazonaws.com:5432/d505ppm9m0iise' + '?ssl=true'
+    connection: 'postgres://localhost/recipes'
   },
 
   production: {
     client: 'pg',
-    connection: 'postgres://iowqbjmosyhglx:7a9588660bb298638c40e5e817ebe025ba2ca1b5eb1b086e59a3ff66d5da6a6c@ec2-54-225-98-131.compute-1.amazonaws.com:5432/d505ppm9m0iise' + '?ssl=true'
+    connection: process.env.NODE_ENV + '?ssl=true'
   }
 
 };
