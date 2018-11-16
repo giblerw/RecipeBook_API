ed69bdffad97df1fc4f400628358f5247352a7ed
GiblerW <westongibler@gmail.com>
2018-11-15 21:17:27 -0700

    added CORS

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express =  require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000
@@ -10,12 +11,14 @@ const app = express();
 
 const route = require('./routes/index');
 
+app.use(cors());
+
 // Logger that outputs all requests into the console
 app.use(morgan('combined'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static('public'))
 // Use v1 as prefix for all API endpoints
 app.use('/', route);
 
